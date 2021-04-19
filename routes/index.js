e8d4692f52c9e003c116199fb456e8fafe967e1d
Haris <souici.haris@gmail.com>
2021-04-19 14:10:05 +0200

    New views / routes
    add new routes / views

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 // Views
+import Album from '../views/Album.jsx'
+import Post from '../views/Post.jsx'
 import Users from '../views/Users.jsx'
 import User from '../views/User.jsx'
 
@@ -14,6 +16,8 @@ export default function Routes() {
 	return (
 		<NavigationContainer>
 			<Stack.Navigator initialRouteName="Users">
+				<Stack.Screen name="Album" component={Album} />
+				<Stack.Screen name="Post" component={Post} />
 				<Stack.Screen name="Users" component={Users} />
 				<Stack.Screen name="User" component={User} />
 			</Stack.Navigator>
